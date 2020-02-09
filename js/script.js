0f27907af5902c53dea5443fe0087b47f58b5ebf
Dmitry-dev777 <57875882+Dmitry-dev777@users.noreply.github.com>
2020-02-09 18:06:59 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+new Vue({
+    el: '#app',
+    data: {
+        items: [
+            'Главная',
+            'Контакты',
+            'Каталог цен',
+            'Instagram'
+        ],
+        menuHamburger: false,
+        menuBuyCoffee: false,
+    }
+});
